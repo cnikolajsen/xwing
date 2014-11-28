5a8282e6caac4156be97126ec0f16be73d485ffb
Christian Nikolajsen <cn@peytz.dk>
2014-11-28 14:14:38 +0100

    Cleaning up the show ship view

--- app/models/faction.rb
+++ app/models/faction.rb
@@ -11,6 +11,7 @@ class Faction < ActiveRecord::Base
   #accepts_nested_attributes_for :faction_pilots, reject_if: proc { |a| a['pilot_id'].blank? }, allow_destroy: true
 
   def ship_names
+    ActionView::Base.send(:include, Rails.application.routes.url_helpers)
     ship_arr = Array.new
     ships.each do |ship|
       ship_arr << ActionController::Base.helpers.link_to("#{ship.ship} - #{ship.pilot_name}", ship)
